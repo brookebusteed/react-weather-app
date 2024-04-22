afea485803ebca6a8c5f079ebc8fa75d2d3d90b5
brookebusteed <brooke_b14@hotmail.com>
2024-04-22 10:07:57 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/brooke-busteed-63046a15a?originalSubdomain=au"
-          target="_blank"
-        >
-          Brooke Busteed{" "}
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/brookebusteed/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/brooke-busteed-63046a15a?originalSubdomain=au"
+            target="_blank"
+          >
+            Brooke Busteed{" "}
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/brookebusteed/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
