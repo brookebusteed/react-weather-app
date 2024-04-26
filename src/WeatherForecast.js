532a957ce008d3bdd9d46b97357a11aef8eca7e8
brookebusteed <brooke_b14@hotmail.com>
2024-04-26 15:21:49 +1000

    Added forecast APi

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+  let longitude = props.coord.lon;
+  let latitude = props.coord.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
